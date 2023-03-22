2dd610eec4296aadcca493e5f881f2300eb5a3de
Foccy <pouneun@naver.com>
2023-03-22 10:37:37 +0900

    api modify

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,2 +1,2 @@
 // export const API_URL = "https://port-0-mango-sever-6g2llfc5dt1p.sel3.cloudtype.app";
-export const API_URL = process.env.NODE_ENV === "production" ? "https://port-0-react-mangoshop-server-6g2llfi8s2ft.sel3.cloudtype.app/" : "http://localhost:8080";
+export const API_URL = process.env.NODE_ENV === "production" ? "https://port-0-react-mangoshop-server-6g2llfi8s2ft.sel3.cloudtype.app" : "http://localhost:8080";
